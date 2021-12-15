d9ce3f04c02177487654d6336ced104f6102ed4a
unknown <usmancuiisb@gmail.com>
2021-12-15 16:44:38 +0500

    initlizing state and react-router-dom task

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import { useEffect , useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [comments, setComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const handleEditClick = (id) => {
+    // history.push(`${id}/edit`)
+  };
+
+  const handleDeleteClick = async (id) => {
+    const result = await axios.delete(
+      'https://jsonplaceholder.typicode.com/posts/' + id
+    );
+    console.log(result.data);
+  };
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
-      setComments(result.data) 
-      
-    }) 
-  } , []);
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((result) => {
+      setComments(result.data);
+    });
+  }, []);
   return (
     <div className="container mt-6">
-      {
-        comments && comments.map(comment=>{
-          return(
-            <div key={comment.id} style={{alignItems:'center',margin:'20px 60px'}}>
+      {comments.map((comment) => {
+        return (
+          <div
+            key={comment.id}
+            style={{ alignItems: 'center', margin: '20px 60px' }}
+          >
             <h4>{comment.id}</h4>
             <h4>{comment.title}</h4>
             <div>
-            <p >{comment.body}</p>
-            <button type="button" className="btn btn-danger mt-3 " >Delete</button>
-            <button type="button" className="btn btn-warning mt-3 ms-3">Edit</button>
-
+              <p>{comment.body}</p>
+              <button
+                type="button"
+                onClick={() => handleDeleteClick(comment.id)}
+                className="btn btn-danger mt-3 "
+              >
+                Delete
+              </button>
+              <button
+                type="button"
+                onClick={() => handleEditClick(comment.id)}
+                className="btn btn-warning mt-3 ms-3"
+              >
+                Edit
+              </button>
             </div>
-            <button type="button" className="btn btn-success mt-3 ms-3">Submit</button>
           </div>
-          )
-
-        })
-      }
+        );
+      })}
     </div>
-  )
+  );
 }
 
-
 export default App;
+
+// react-router-dom
+
+// redirect using history object of rrd
