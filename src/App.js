24c8e3b3ebe7595e8ca7918b33a219186c867c1a
waqas-bloch <waq77443@gmail.com>
2021-12-15 16:51:52 +0500

    simple push

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   const [comments, setComments] = useState(false);
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
-      setComments(result.data) 
-      
+    axios.get("https://jsonplaceholder.typicode.com/posts/1").then((result) => {
+      //setComments(result.data) 
+      console.log(result.data);cd
     }) 
   } , []);
   return (
@@ -22,9 +22,7 @@ function App() {
             <p >{comment.body}</p>
             <button type="button" className="btn btn-danger mt-3 " >Delete</button>
             <button type="button" className="btn btn-warning mt-3 ms-3">Edit</button>
-
             </div>
-            <button type="button" className="btn btn-success mt-3 ms-3">Submit</button>
           </div>
           )
 
